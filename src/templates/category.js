378e0d7d5e3a41125e3254f8061d9f3c550f2e7d
paras <paras@qltech.com.au>
2019-09-17 13:11:11 +0530

    second commit

--- src/templates/category.js
+++ src/templates/category.js
@@ -30,7 +30,7 @@ export const pageQuery = graphql`
         title
       }
     }
-    allWordpressPost(filter: { categories: { slug: { eq: $slug } } }) {
+    allWordpressPost(filter: { categories: { elemMatch:{ slug: { eq: $slug }} } }) {
       totalCount
       edges {
         node {

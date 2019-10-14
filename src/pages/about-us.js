1301088b90d809db785aff9551e2c4099a54c025
paras <paras@qltech.com.au>
2019-10-14 17:24:45 +0530

    14-10 05:25 PM Commit

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -218,7 +218,7 @@ const AboutPage = () => (
                                        </div>
                                </div>
                     </div>
-                </div>
+                
                
            
 

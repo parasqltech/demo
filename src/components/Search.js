331478f664b747588dbce3249075a5ce5c2c6f56
paras <paras@qltech.com.au>
2019-10-14 14:03:44 +0530

    14-10 02:04 pm Commit

---
+++ src/components/Search.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react"
+import { Index } from "elasticlunr"
+ import { Link, StaticQuery, graphql } from 'gatsby'
+// Search component
+export default class Search extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      query: ``,
+      results: [],
+    }
+	
+  }
+ 
+  render() {
+    return (
+      <>
+        <input type="text" className="form-control" placeholder="Search Here..." value={this.state.query} onChange={this.search} />
+        <ul className="lisearch" >
+          {this.state.results.map(page => (
+            <li key={page.node.id}>
+              <Link to={"/" + page.node.url.replace('https://qltech.io/','/hub/')}>{page.node.title}</Link>
+              
+            </li>
+          ))}
+        </ul>
+      </>
+    )
+  }
+  getOrCreateIndex = () =>
+    this.index
+      ? this.index
+      : // Create an elastic lunr index and hydrate with graphql query results
+        Index.load(this.props.searchIndex)
+ 
+  search = evt => {
+    const query = evt.target.value;
+	this.setState({query: query});
+	
+	let filtered=this.props.searchIndex.filter((item)=>{
+      return item.node.title.toLowerCase().indexOf(query.toLowerCase()) > -1
+    });
+    if (query === "") {
+      filtered = [];
+    }
+    this.setState({
+      filtered
+    })
+	
+	console.log(filtered);
+	this.setState({results: filtered});
+
+  }
+}

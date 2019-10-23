4416114cfe551898ebeee1cd8a3493809b0426a3
paras <paras@qltech.com.au>
2019-10-23 11:18:29 +0530

    23-10 11:18 AM Commit

--- src/components/AboutUs/AboutFirstSection.js
+++ src/components/AboutUs/AboutFirstSection.js
@@ -37,24 +37,21 @@ render() {
             <div className="container">
                 <div className="main-panel">
                     <div className="row">
-                        <div className="col-lg-5 mb-4">
+                        <div className="col-lg-4 mb-4">
                             <div className="play-video-thumbnial">
                                 <img src={about_top} className="img-fluid " alt=""/>
-                                <div className="play-button" onClick={() => this.handleModalOpen()}>
-                                    <span className="video-area-popup">
-                                        <i className="fa fa-play"></i>
-                                    </span>
-                                </div>
+                                
                             
                             </div>
                         </div>
-                        <div className="col-lg-7">
+                        <div className="col-lg-8">
                             <p className="mb-0 sub-heading">How we work</p>
                             <h2 className="section-heading">We help transform and grow businesses for the next </h2>
                             <p className="label-text">
                                     We offer end-to-end solutions to help transform businesses - strategise, develop, automate &amp; grow the brand’s online. In collaboration with our clients, we’ve successfully managed to help businesses increase productivity, reduce costs, enhance agility & performance.
 
                             </p>
+							<div class="play-button" onClick={() => this.handleModalOpen()}><span data-video-id="" class="video-area-popup"><i class="fa fa-play"></i></span><span>Check out our quick introduction</span></div>
                         </div>
                     </div>
                 </div>
